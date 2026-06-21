1fbac77990d42733f7882323d953634710dac86f
hadi moradi <hm@kosmonaut.io>
2026-06-21 17:31:25 +0200

    Install twig/intl-extra package and display the comments per conference page.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,23 +2,34 @@
 
 namespace App\Controller;
 
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-            EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
+    #[Route('/conference/{id}', name: 'conference_show')]
+    public function show(Environment $twig, string $id, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        // foreach($conferenceRepository->find($id)->getComments() as $comment) {
+        //     dump($comment);
+        // }die;
+        // dump($conferenceRepository->find($id)->getComments());die;
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference]),
+        ]));
     }
 }
